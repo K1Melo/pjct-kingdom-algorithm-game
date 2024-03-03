8fcd87a70b9f270bbab24b7b0900b7211721d3c2
K1Melo <kauafernandes.smelo@gmail.com>
2024-03-02 23:48:01 -0300

    feat: movimentation and parallax efect

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,54 +1,52 @@
 extends CharacterBody2D
 
-const SPEED = 150.0
-const JUMP_FORCE = -400.0
-var current_dir = "none"
-var jumping = false
+#player movement variable
+@export var speed = 150
+@export var gravity = 500
+@export var jump_height = -250
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#jump states
+var is_jumping:bool = false
 
-
-func _physics_process(_delta):
-	if not is_on_floor():
-		velocity.y += gravity * _delta
-		jumping = true
-	if is_on_floor():
-		jumping = false
-		
-	_move_system(_delta)
-
-func _move_system(_delta):
-	if Input.is_action_pressed("move_right"):
-		current_dir = "right"
-		play_anim(1)
-		velocity.x = SPEED
-	elif Input.is_action_pressed("move_left"):
-		current_dir = "left"
-		play_anim(1)
-		velocity.x = -SPEED
-	else:
-		play_anim(0)
-		velocity.x = 0
-	if Input.is_action_just_pressed("jump") and !jumping:
-		velocity.y = JUMP_FORCE
-		
-	move_and_slide()
+func _physics_process(delta):
+	velocity.y += gravity * delta
 	
-func play_anim(movement):
-	var dir = current_dir
-	var anim = $sprites
-	
-	if dir == "right":
-		anim.flip_h = false
-		if movement == 1:
-			anim.play("move")
-		elif movement == 0:
-			anim.play("idle")
-	if dir == "left":
-		anim.flip_h = true
-		if movement == 1:
-			anim.play("move")
-		elif movement == 0:
-			anim.play("idle")
+	_movement()
+	move_and_slide() 
 
+func _movement():
+	var _input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	velocity.x = _input * speed
+	play_anim()
+	
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			is_jumping = true
+			velocity.y = jump_height
+			play_anim()
+
+func play_anim():
+	var _anim = $sprites
+	
+	#Idle animation
+	if !Input.is_anything_pressed() && !is_jumping:
+		_anim.play("idle")
+		
+	if !is_jumping:
+		#Right animation
+		if Input.is_action_pressed("move_right"):
+			_anim.play("move")
+			_anim.flip_h = false
+		#Left animation
+		if Input.is_action_pressed("move_left"):
+			_anim.flip_h = true
+			_anim.play("move")
+			
+	if !is_on_floor():
+		if velocity.y < 0:
+			$sprites.play("jump")
+		else:
+			$sprites.play("fall")
+
+func _on_sprites_animation_finished():
+	is_jumping = false
